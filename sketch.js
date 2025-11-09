270cc060a697de6be8c8520b2aacadc9beb13c1a
BochengMeng <bochengmeng99@gmail.com>
2025-11-09 22:17:57 +1100

    canvas set&img

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
-function setup() {
-  createCanvas(400, 400);
+let sourceImage;
+function preload() {
+  // load image https://p5js.org/reference/p5/preload/
 }
-
+function setup() {
+  createCanvas(600, 600);
+}
 function draw() {
-  background(220);
+  background(255);
 }
