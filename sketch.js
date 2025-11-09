2fc8be814fa1c33beeabf4bcb8984a8e035e8f6b
BochengMeng <bochengmeng99@gmail.com>
2025-11-09 22:25:19 +1100

    draw img test

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let sourceImage;
 function preload() {
+  img = loadImage('Street.png');
   // load image https://p5js.org/reference/p5/preload/
 }
 function setup() {
@@ -7,4 +8,6 @@ function setup() {
 }
 function draw() {
   background(255);
+  // Draw the image.
+  image(img, 0, 0);
 }
